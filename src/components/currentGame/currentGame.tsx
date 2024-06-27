2debfe0f58f0fcd10e4e309c35d479d786f30a3f
JaxV9 <layanj.pro@gmail.com>
2024-06-27 17:49:14 +0200

    feat: working on currentGame

--- src/components/currentGame/currentGame.tsx
+++ src/components/currentGame/currentGame.tsx
@@ -4,7 +4,14 @@ export const CurrentGame = () => {
 
     return(
         <>
-            
+            <div className="currentGameContainer">
+                <div className="currentGamePictureContainer">
+                    <span>Jeu récemment joué</span>
+                    <div className="currentGamePicture" style={{backgroundImage: "url(/assets/eldenring.jpg"}}>
+                    </div>
+                </div>
+                <div className="cureentGameChallengeContainer"></div>
+            </div>
         </>
     )
 }
