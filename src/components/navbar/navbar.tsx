6022f20d3c61ce9b394389438b02215f1d8ad006
JaxV9 <layanj.pro@gmail.com>
2024-06-27 13:04:53 +0200

    feat: navBarAdded

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,7 +5,9 @@ export const Navbar = () => {
     return(
         <>
             <div className="navBarContainer">
-                
+                    <div className="homeIcon"></div>
+                    <div className="challengesIcon"></div>
+                    <div className="rewardsIcon"></div>
             </div>
         </>
     )
