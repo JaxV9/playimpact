586cca495c32d957f815510fee8e01b1e39b5ed6
JaxV9 <layanj.pro@gmail.com>
2024-06-28 10:52:34 +0200

    feat: challengesList did

--- src/components/challengesList/challengesList.tsx
+++ src/components/challengesList/challengesList.tsx
@@ -21,7 +21,7 @@ export const ChallengesList = () => {
                                 </div>
                             </div>
                         </div>
-                        <span className="challengeLabel">Soumettez une photo de l'appareil avec le reçu ou la confirmation d'achat de la plateforme de reconditionnement.
+                        <span className="challengeLabel">Soumettez une photo de l&apos;appareil avec le reçu ou la confirmation d&apos;achat de la plateforme de reconditionnement.
                         </span>
                     </div>
                 </div>
@@ -29,19 +29,20 @@ export const ChallengesList = () => {
                     <div className="challengeContentContainer">
                         <div className="challenge">
                             <div className="challengePictureContainer">
-                                <div className="challengePicture" style={{ backgroundImage: "url(/assets/reconditionné.png" }}>
+                                <div className="challengePicture" style={{ backgroundImage: "url(/assets/video-games.png" }}>
                                 </div>
                                 <div className="tagDifficulty">Facile</div>
                             </div>
                             <div className="thisChallengeContainer">
-                                <span className="descrChallenge">Achetez un appareil électronique reconditionné</span>
-                                <span className="labelProgressBar">60%</span>
+                                <span className="descrChallenge">Revendez trois jeux vidéo</span>
+                                <span className="labelProgressBar">66%</span>
                                 <div className="progressBarContainerChallenge">
                                     <div className="progressBarChallenge" style={{ width: "60%" }}></div>
                                 </div>
                             </div>
                         </div>
-                        <span className="challengeLabel">Soumettez une photo de l'appareil avec le reçu ou la confirmation d'achat de la plateforme de reconditionnement.
+                        <span className="challengeLabel">
+                            Soumettez une photo des articles déposés au centre de recyclage avec une attestation ou un reçu du centre.
                         </span>
                     </div>
                 </div>
@@ -49,19 +50,20 @@ export const ChallengesList = () => {
                     <div className="challengeContentContainer">
                         <div className="challenge">
                             <div className="challengePictureContainer">
-                                <div className="challengePicture" style={{ backgroundImage: "url(/assets/reconditionné.png" }}>
+                                <div className="challengePicture" style={{ backgroundImage: "url(/assets/antibacterien.png" }}>
                                 </div>
                                 <div className="tagDifficulty">Facile</div>
                             </div>
                             <div className="thisChallengeContainer">
-                                <span className="descrChallenge">Achetez un appareil électronique reconditionné</span>
+                                <span className="descrChallenge">Effectuez un nettoyage complet et un entretien de votre matériel de jeu (console, PC, manette, etc.) pour prolonger sa durée de vie et améliorer son efficacité.</span>
                                 <span className="labelProgressBar">60%</span>
                                 <div className="progressBarContainerChallenge">
                                     <div className="progressBarChallenge" style={{ width: "60%" }}></div>
                                 </div>
                             </div>
                         </div>
-                        <span className="challengeLabel">Soumettez une photo de l'appareil avec le reçu ou la confirmation d'achat de la plateforme de reconditionnement.
+                        <span className="challengeLabel">
+                            Soumettez des photos avant et après du matériel nettoyé et entretenu, accompagnées d&apos;une courte description de ce qui a été fait sur Instagram.
                         </span>
                     </div>
                 </div>
@@ -69,19 +71,20 @@ export const ChallengesList = () => {
                     <div className="challengeContentContainer">
                         <div className="challenge">
                             <div className="challengePictureContainer">
-                                <div className="challengePicture" style={{ backgroundImage: "url(/assets/reconditionné.png" }}>
+                                <div className="challengePicture" style={{ backgroundImage: "url(/assets/plage.png" }}>
                                 </div>
                                 <div className="tagDifficulty">Facile</div>
                             </div>
                             <div className="thisChallengeContainer">
-                                <span className="descrChallenge">Achetez un appareil électronique reconditionné</span>
+                                <span className="descrChallenge">Nettoyez une plage avec l&apos;association INITIATIVES OCÉANES.</span>
                                 <span className="labelProgressBar">60%</span>
                                 <div className="progressBarContainerChallenge">
                                     <div className="progressBarChallenge" style={{ width: "60%" }}></div>
                                 </div>
                             </div>
                         </div>
-                        <span className="challengeLabel">Soumettez une photo de l'appareil avec le reçu ou la confirmation d'achat de la plateforme de reconditionnement.
+                        <span className="challengeLabel">
+                            Scannez le QR code auprès des bénévoles de l&apos;association pour confirmer votre action.
                         </span>
                     </div>
                 </div>
@@ -89,39 +92,20 @@ export const ChallengesList = () => {
                     <div className="challengeContentContainer">
                         <div className="challenge">
                             <div className="challengePictureContainer">
-                                <div className="challengePicture" style={{ backgroundImage: "url(/assets/reconditionné.png" }}>
+                                <div className="challengePicture" style={{ backgroundImage: "url(/assets/aide.png" }}>
                                 </div>
                                 <div className="tagDifficulty">Facile</div>
                             </div>
                             <div className="thisChallengeContainer">
-                                <span className="descrChallenge">Achetez un appareil électronique reconditionné</span>
+                                <span className="descrChallenge">Faire du bénévolat auprès de la Croix Rouge / les Restos du Coeur.</span>
                                 <span className="labelProgressBar">60%</span>
                                 <div className="progressBarContainerChallenge">
                                     <div className="progressBarChallenge" style={{ width: "60%" }}></div>
                                 </div>
                             </div>
                         </div>
-                        <span className="challengeLabel">Soumettez une photo de l'appareil avec le reçu ou la confirmation d'achat de la plateforme de reconditionnement.
-                        </span>
-                    </div>
-                </div>
-                <div className="challengeContainer">
-                    <div className="challengeContentContainer">
-                        <div className="challenge">
-                            <div className="challengePictureContainer">
-                                <div className="challengePicture" style={{ backgroundImage: "url(/assets/reconditionné.png" }}>
-                                </div>
-                                <div className="tagDifficulty">Facile</div>
-                            </div>
-                            <div className="thisChallengeContainer">
-                                <span className="descrChallenge">Achetez un appareil électronique reconditionné</span>
-                                <span className="labelProgressBar">60%</span>
-                                <div className="progressBarContainerChallenge">
-                                    <div className="progressBarChallenge" style={{ width: "60%" }}></div>
-                                </div>
-                            </div>
-                        </div>
-                        <span className="challengeLabel">Soumettez une photo de l'appareil avec le reçu ou la confirmation d'achat de la plateforme de reconditionnement.
+                        <span className="challengeLabel">
+                            Scannez le QR code auprès des bénévoles de l&apos;association pour confirmer votre action.
                         </span>
                     </div>
                 </div>
